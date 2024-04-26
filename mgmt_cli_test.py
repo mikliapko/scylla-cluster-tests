aa9a966088f92d389d5579ac864d018ee02e18cf
Liapkovich <mikita.liapkovich@scylladb.com>
2024-04-26 17:13:19 +0200

    fix(manager): test commit

--- mgmt_cli_test.py
+++ mgmt_cli_test.py
@@ -543,7 +543,9 @@ class MgmtCliTest(BackupFunctionsMixIn, ClusterTester):
         mgr_cluster = manager_tool.get_cluster(cluster_name=self.CLUSTER_NAME) \
             or manager_tool.add_cluster(name=self.CLUSTER_NAME, db_cluster=self.db_cluster,
                                         auth_token=self.monitors.mgmt_auth_token)
-        backup_task = mgr_cluster.create_backup_task(location_list=self.locations, keyspace_list=["keyspace1"])
+        location_list = ["us-west-2scylla_node_west:s3:manager-backup-tests-us-west-2",
+                         "us-eastscylla_node_east:s3:manager-backup-tests-us-east-1"]
+        backup_task = mgr_cluster.create_backup_task(location_list=location_list, keyspace_list=["keyspace1"])
         backup_task_status = backup_task.wait_and_get_final_status(timeout=1500)
         assert backup_task_status == TaskStatus.DONE, \
             f"Backup task ended in {backup_task_status} instead of {TaskStatus.DONE}"
@@ -553,8 +555,6 @@ class MgmtCliTest(BackupFunctionsMixIn, ClusterTester):
         # location = self.locations[0]
         # datacenters = set(node.datacenter for node in self.db_cluster.nodes)
         # location_list = [f"{dc}:{location}" for dc in datacenters]
-
-        location_list = ["us-west-2scylla_node_west:s3:manager-backup-tests-us-east-1"]
 
         with adaptive_timeout(Operations.MGMT_REPAIR, self.db_cluster.nodes[0], timeout=soft_timeout):
             self.verify_backup_success(mgr_cluster=mgr_cluster, backup_task=backup_task, restore_data_with_task=True,
