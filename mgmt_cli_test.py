59252f672f71aeb7f17733c3dabec60b2657b076
Liapkovich <mikita.liapkovich@scylladb.com>
2024-05-16 17:27:25 +0200

    feature(manager): manager installation test

--- mgmt_cli_test.py
+++ mgmt_cli_test.py
@@ -1147,3 +1147,26 @@ class MgmtCliTest(BackupFunctionsMixIn, ClusterTester):
                 f'Task {current_task_status} did not remain in "{TaskStatus.STOPPED}" status, but instead ' \
                 f'reached "{current_task_status}" status'
         self.log.info('finishing test_suspend_and_resume_without_starting_tasks')
+
+    def test_manager_installed_and_functional(self):
+        """Verify that the manager is installed and functional.
+
+        The test is intended for execution on non-main OS distributions (for example, debian10)
+        where the main goal is to execute installation test.
+        The rest of the checks are executed on Ubuntu distribution in test_manager_sanity.
+        """
+        self.log.info('starting test_manager_installed_and_functional')
+
+        manager_node = self.monitors.nodes[0]
+        scylla_node = self.db_cluster.nodes[0]
+
+        # Check scylla-manager is up and running
+        manager_node.is_manager_server_up()
+
+        # Check scylla-manager-agent is up and running
+        scylla_node.is_manager_agent_up()
+
+        # Check the sctool version method is callable
+        _ = mgmt.get_scylla_manager_tool(manager_node=manager_node).sctool.version
+
+        self.log.info('finishing test_manager_installed_and_functional')
