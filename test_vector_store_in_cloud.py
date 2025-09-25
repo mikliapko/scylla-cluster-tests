603f4e9453e4bca0962ab82e44104905bae13d7d
Liapkovich <mikita.liapkovich@scylladb.com>
2025-09-25 13:37:41 +0200

    test(vector_store): add replace node tests - test for broken node

--- test_vector_store_in_cloud.py
+++ test_vector_store_in_cloud.py
@@ -5,6 +5,8 @@ from functools import cached_property
 from threading import Thread
 from typing import Optional
 
+import cassandra
+
 from sdcm.mgmt.helpers import get_schema_create_statements_from_file, substitute_dc_name_in_ks_statements_if_different
 from sdcm.tester import ClusterTester
 from sdcm.utils.common import S3Storage
@@ -94,48 +96,48 @@ class VectorStoreInCloudBase(ClusterTester):
 
     def test_vector_store_recall(self, queries_num: Optional[int] = None, duration: Optional[int] = None):
 
+        assert (queries_num is not None) ^ (duration is not None), \
+            "Either queries_num or duration must be provided, but not both"
+
         total_recall = 0
 
-        def _run_ann_search():
-            nonlocal total_recall
-            index = random.randint(0, len(self.test_data) - 1)
-            vector_value = self.test_data[index]
+        with self.db_cluster.cql_connection_patient(self.db_cluster.nodes[0], verbose=False) as session:
+            def _run_ann_search():
+                nonlocal total_recall
+                index = random.randint(0, len(self.test_data) - 1)
+                vector_value = self.test_data[index]
 
-            with self.db_cluster.cql_connection_patient(self.db_cluster.nodes[0]) as session:
                 query = ANN_SEARCH_CMD_TEMPLATE.format(vector=vector_value)
                 self.log.debug("Running verification request #%d", query_num)
                 rows = session.execute(query).all()
 
-            actual_ids = [row.id for row in rows]
-            ground_truth_ids = ast.literal_eval(self.ground_truth_data[index])[:10]
-            assert len(actual_ids) == len(ground_truth_ids), \
-                f"Mismatch in number of returned IDs ({len(actual_ids)}) and ground truth IDs ({len(ground_truth_ids)})"
+                actual_ids = [row.id for row in rows]
+                ground_truth_ids = ast.literal_eval(self.ground_truth_data[index])[:10]
+                assert len(actual_ids) == len(ground_truth_ids), \
+                    f"Mismatch in number of returned IDs ({len(actual_ids)}) and ground truth IDs ({len(ground_truth_ids)})"
 
-            # Count how many elements from actual_ids are present in ground_truth_ids (order doesn't matter)
-            elements_present = sum(1 for actual_id in actual_ids if actual_id in ground_truth_ids)
+                # Count how many elements from actual_ids are present in ground_truth_ids (order doesn't matter)
+                elements_present = sum(1 for actual_id in actual_ids if actual_id in ground_truth_ids)
 
-            query_recall = elements_present / len(actual_ids)
-            total_recall += query_recall
+                query_recall = elements_present / len(actual_ids)
+                total_recall += query_recall
 
-            self.log.debug("Query #%d: %d/%d elements present, recall factor: %.2f",
-                           query_num, elements_present, len(actual_ids), query_recall)
+                self.log.debug("Query #%d: %d/%d elements present, recall factor: %.2f",
+                               query_num, elements_present, len(actual_ids), query_recall)
 
-        assert (queries_num is not None) ^ (duration is not None), \
-            "Either queries_num or duration must be provided, but not both"
+            if queries_num is not None:
+                for query_num in range(1, queries_num + 1):
+                    _run_ann_search()
 
-        if queries_num is not None:
-            for query_num in range(1, queries_num + 1):
-                _run_ann_search()
+                overall_queries_num = queries_num
+            else:
+                query_num = 0
+                end_time = time.time() + duration
+                while time.time() < end_time:
+                    query_num += 1
+                    _run_ann_search()
 
-            overall_queries_num = queries_num
-        else:
-            query_num = 0
-            end_time = time.time() + duration
-            while time.time() < end_time:
-                query_num += 1
-                _run_ann_search()
-
-            overall_queries_num = query_num
+                overall_queries_num = query_num
 
         average_recall = round(total_recall / overall_queries_num, 3)
         self.average_recall = average_recall
@@ -154,14 +156,14 @@ class VectorStoreInCloudSanity(VectorStoreInCloudBase):
 
 
 class VectorStoreInCloudReplaceNode(VectorStoreInCloudBase):
-    def test_replace_node_no_interruptions_expected(self,
-                                                    instance_type_id: Optional[int] = None,
-                                                    version: Optional[str] = None):
+    def test_replace_healthy_node(self,
+                                  server_id: str,
+                                  instance_type_id: Optional[int] = None,
+                                  version: Optional[str] = None):
         self.log.debug("Start Vector Store verification queries in background thread")
         recall_test_thread = Thread(target=self.test_vector_store_recall, kwargs={"duration": 300})
         recall_test_thread.start()
 
-        server_id = random.choice(self.db_cluster.define_vector_store_node_ids())
         self.log.debug("Replace Vector Store node %s - new instance type ID %s, new version %s",
                        server_id, instance_type_id, version)
         self.db_cluster.replace_vector_store_node(
@@ -172,28 +174,65 @@ class VectorStoreInCloudReplaceNode(VectorStoreInCloudBase):
 
         recall_test_thread.join(timeout=300)
 
-    def test_replace_node_broken_instance(self):
+    def test_replace_broken_node(self, server_id: str):
         vector_value = random.choice(self.test_data)
         query = ANN_SEARCH_CMD_TEMPLATE.format(vector=vector_value)
 
-        # It will be run in background while node replacement is in progress
-        with self.db_cluster.cql_connection_patient(self.db_cluster.nodes[0]) as session:
-            session.execute(query)
-            time.sleep(1)
+        failed_queries_num = 0
+
+        def _run_ann_search():
+            nonlocal failed_queries_num
+            with self.db_cluster.cql_connection_patient(self.db_cluster.nodes[0], verbose=False) as session:
+                while True:
+                    try:
+                        session.execute(query)
+                    except cassandra.InvalidRequest as e:
+                        self.log.warning(f"ANN search request failed: {e}")
+                        failed_queries_num += 1
+                    time.sleep(1)
+
+        ann_search_thread = Thread(target=_run_ann_search)
+        ann_search_thread.start()
+
+        self.log.info("vector-store should be stopped MANUALLY on server ID %s, sleeping 5 mins...", server_id)
+        time.sleep(300)
+
+        failed_before_replacement = failed_queries_num
+        self.log.debug("Number of failed requests BEFORE node replacement: %d", failed_before_replacement)
+        assert failed_before_replacement > 0, "ANN search queries didn't fail after stopping vector-store service"
+
+        self.log.info("vector-store service should be stopped MANUALLY on server ID %s", server_id)
+        self.db_cluster.replace_vector_store_node(server_id=server_id)
+
+        failed_after_replacement = failed_queries_num
+        self.log.debug("Number of failed requests AFTER node replacement: %d", failed_after_replacement)
+        assert failed_after_replacement > failed_before_replacement, \
+            "ANN search queries didn't fail during node replacement"
+
+        ann_search_thread.join(timeout=120)
+        assert failed_after_replacement == failed_queries_num, "ANN search queries are still failing after node replace"
 
     def test_vector_store_replace_node_in_cloud(self):
         self.log.info("Prepare Vector Store data")
         self.prepare_vector_store_index()
 
-        self.log.info("Run Vector Store verification BEFORE replacing a node")
-        self.test_vector_store_recall(queries_num=10)
+        vs_nodes_server_ids = self.db_cluster.define_vector_store_node_ids()
 
-        self.log.info("Run VS node replacement, no changes to instance type or version")
-        self.test_replace_node_no_interruptions_expected()
+        test = "Run VS node replacement, no changes to instance type or version"
+        with self.subTest(test):
+            self.log.info(test)
+            self.test_replace_healthy_node(server_id=vs_nodes_server_ids[0])
 
-        self.log.info("Run node replacement changing instance type")
-        instance_type_id = 62 if self.params.get("cluster_backend") == "aws" else 41
-        self.test_replace_node_no_interruptions_expected(instance_type_id=instance_type_id)
+        test = "Run VS node replacement changing instance type"
+        with self.subTest(test):
+            self.log.info(test)
+            instance_type_id = 62 if self.params.get("cluster_backend") == "aws" else 41
+            self.test_replace_healthy_node(
+                server_id=vs_nodes_server_ids[1],
+                instance_type_id=instance_type_id,
+            )
 
-        self.log.info("Run Vector Store verification AFTER replacing a node")
-        self.test_vector_store_recall(queries_num=10)
+        test = "Run VS node replacement for broken instance"
+        with self.subTest(test):
+            self.log.info(test)
+            self.test_replace_broken_node(server_id=vs_nodes_server_ids[2])
