a13ee4c869e9a06ca2750b4d1d9f0dbb270c1cd4
Liapkovich <mikita.liapkovich@scylladb.com>
2025-11-06 11:39:40 +0100

    fix(vector-search): refactor sanity test and add configuration for cloud

--- test_vector_search_sanity.py
+++ test_vector_search_sanity.py
@@ -1,6 +1,7 @@
 import time
 
 from sdcm.tester import ClusterTester
+from sdcm.utils import loader_utils
 from sdcm.utils.common import S3Storage
 
 BUCKET_NAME = "vector-store-in-cloud"
@@ -10,7 +11,7 @@ GROUND_TRUTH_FILENAME = "ground_truth.txt"
 TEST_DATA_FILENAME = "test_data.txt"
 
 
-class VectorSearchBase(ClusterTester):
+class VectorSearchSanity(ClusterTester, loader_utils.LoaderUtilsMixin):
     @staticmethod
     def download_vector_search_test_data_from_s3():
         s3_storage = S3Storage(bucket=BUCKET_NAME)
@@ -20,25 +21,20 @@ class VectorSearchBase(ClusterTester):
         s3_storage.download_file(link=f"{base_url}/{GROUND_TRUTH_FILENAME}", dst_dir=LATTE_DIR_PATH)
         s3_storage.download_file(link=f"{base_url}/{DATASET_FILENAME}", dst_dir=LATTE_DIR_PATH)
 
-
-class VectorSearchSanity(VectorSearchBase):
     def test_vector_search_sanity(self):
         self.log.info("Prepare Vector Search data")
         self.download_vector_search_test_data_from_s3()
 
-        self.log.info("Create schema and index")
-        command = f"latte schema {LATTE_DIR_PATH}/vector_search.rn"
-        self.run_stress_thread(command)
-
-        time.sleep(3600)
-
         self.log.info("Populate cluster with data")
-        command = f"latte run {LATTE_DIR_PATH}/vector_search.rn -f insert -d 50000"
-        self.run_stress_thread(command)
+        self.run_prepare_write_cmd()
 
         self.log.info("Wait for index to build")
-        time.sleep(120)
+        time.sleep(3600)
 
         self.log.info("Run ANN OF queries and validate recall")
-        command = f"latte run {LATTE_DIR_PATH}/vector_search.rn -f validate_average_recall -d 60s"
-        self.run_stress_thread(command)
+        stress_queue = []
+        stress_cmd = self.params.get('stress_cmd')
+        self.assemble_and_run_all_stress_cmd(stress_queue, stress_cmd, keyspace_num=1)
+
+        for stress in stress_queue:
+            self.verify_stress_thread(stress)
